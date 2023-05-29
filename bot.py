90f994570093b3b0a083407501550e0a8854eac7
ikromjon008 <129050303+ikromjon008@users.noreply.github.com>
2023-05-29 10:26:07 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+mport telebot
+
+
+# bot father yordamida yaratgan botimizni tokenini olamiz
+TOKEN = "5773060087:AAHpLmsAqn4b8eIQaL997SX85N7wRE5R2WQ"
+
+
+bot = telebot.TeleBot(TOKEN)
+
+# botga start buyruyrug'ini yuborganda funksiyani bajaradi.
+@bot.message_handler(commands=['start'])
+
+
+# hisoblashni boshlash uchun har xil buyruqlarni bajaramiz
+def boshlov(message):
+	if (message.text == '/start'):
+		bot.reply_to(message, "Assalomu alaykum botimizga xush keldingiz! ")
+    
+bot.polling(none_stop=True)
